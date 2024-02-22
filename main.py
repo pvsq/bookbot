3b819e40ec0f63b1249f65124b896190ab9af58c
pvsq <pv_sqra@protonmail.com>
2024-02-22 09:39:24 +0530

    main.py: Count words in text

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
     filename = "books/frankenstein.txt"
     file_contents = get_file_contents(filename)
-    print(file_contents)
-    print(f.closed)
+    word_count = count_words(file_contents)
+    print(f"{word_count} words found")
 
 def get_file_contents(filename):
     with open(filename) as f:
         file_contents = f.read()
     return file_contents
+
+def count_words(text):
+    words = text.split()
+    word_count = len(words)
+    return word_count
